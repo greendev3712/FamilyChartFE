d8fc793ea7d7c8ec8d71de0f9faf83836d422d48
greendev3712 <businessxfreeman@gmail.com>
2023-02-22 22:32:13 +0100

    complete

--- assets/js/descendants-chart/modules/chart.js
+++ assets/js/descendants-chart/modules/chart.js
@@ -1,17 +1,16 @@
 /**
- * See LICENSE.md file for further details.
+ * This file is part of the package magicsunday/webtrees-descendants-chart.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
-
 import * as d3 from "./d3";
-import Configuration from "./configuration";
 import Hierarchy from "./chart/hierarchy";
+import Tree from "./tree";
 import Overlay from "./chart/overlay";
 import Svg from "./chart/svg";
-import Person from "./chart/svg/person";
-import Gradient from "./chart/gradient";
-import Update from "./chart/update";
 
-const MIN_HEIGHT  = 500;
+const MIN_HEIGHT  = 300;
 const MIN_PADDING = 10;   // Minimum padding around view box
 
 /**
@@ -19,7 +18,7 @@ const MIN_PADDING = 10;   // Minimum padding around view box
  *
  * @author  Rico Sonntag <mail@ricosonntag.de>
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License v3.0
- * @link    https://github.com/magicsunday/webtrees-fan-chart/
+ * @link    https://github.com/magicsunday/webtrees-descendants-chart/
  */
 export default class Chart
 {
@@ -40,7 +39,7 @@ export default class Chart
     /**
      * Returns the SVG instance.
      *
-     * @return {Svg}
+     * @returns {Svg}
      */
     get svg()
     {
@@ -56,7 +55,6 @@ export default class Chart
     {
         // Get bounding boxes
         let svgBoundingBox    = this._svg.visual.node().getBBox();
-
         let clientBoundingBox = this._parent.node().getBoundingClientRect();
 
         // View box should have at least the same width/height as the parent element
@@ -75,6 +73,13 @@ export default class Chart
         viewBoxWidth  = Math.ceil(viewBoxWidth + (MIN_PADDING * 2));
         viewBoxHeight = Math.ceil(viewBoxHeight + (MIN_PADDING * 2));
 
+        // this._svg.visual
+        //     .attr("transform", "translate(" + (-viewBoxLeft) + ", " + (-viewBoxTop) + ")");
+        //
+        // this._svg.get()
+        //     .attr("width", viewBoxWidth)
+        //     .attr("height", viewBoxHeight);
+
         // Set view box attribute
         this._svg.get()
             .attr("viewBox", [
@@ -83,27 +88,12 @@ export default class Chart
                 viewBoxWidth,
                 viewBoxHeight
             ]);
-
-        // Add rectangle element
-        // this._svg
-        //     .insert("rect", ":first-child")
-        //     .attr("class", "background")
-        //     .attr("width", "100%")
-        //     .attr("height", "100%")
-        //     .style("fill", "none")
-        //     .style("pointer-events", "all");
-        //
-        // // Adjust rectangle position
-        // this._svg
-        //     .select("rect")
-        //     .attr("x", viewBoxLeft)
-        //     .attr("y", viewBoxTop);
     }
 
     /**
      * Returns the chart data.
      *
-     * @return {Object}
+     * @returns {Object}
      */
     get data()
     {
@@ -140,30 +130,32 @@ export default class Chart
         // Init the <svg> events
         this._svg.initEvents(this._overlay);
 
-        let personGroup = this._svg.get().select("g.personGroup");
-        let gradient    = new Gradient(this._svg, this._configuration);
-        let that        = this;
+        let tree = new Tree(this._svg, this._configuration, this._hierarchy);
 
-         personGroup
-            .selectAll("g.person")
-            .data(this._hierarchy.nodes, (d) => d.data.id)
-            .enter()
-            .append("g")
-            .attr("class", "person")
-            .attr("id", (d) => "person-" + d.data.id);
-
-        // Create a new selection in order to leave the previous enter() selection
-        personGroup
-            .selectAll("g.person")
-            .each(function (d) {
-                let person = d3.select(this);
-
-                if (that._configuration.showColorGradients) {
-                    gradient.init(d);
-                }
-
-                new Person(that._svg, that._configuration, person, d);
-            });
+        // let personGroup = this._svg.get().select("g.personGroup");
+        // let gradient    = new Gradient(this._svg, this._configuration);
+        // let that        = this;
+        //
+        // personGroup
+        //     .selectAll("g.person")
+        //     .data(this._hierarchy.nodes, (d) => d.data.id)
+        //     .enter()
+        //     .append("g")
+        //     .attr("class", "person")
+        //     .attr("id", (d) => "person-" + d.data.id);
+        //
+        // // Create a new selection in order to leave the previous enter() selection
+        // personGroup
+        //     .selectAll("g.person")
+        //     .each(function (d) {
+        //         let person = d3.select(this);
+        //
+        //         if (that._configuration.showColorGradients) {
+        //             gradient.init(d);
+        //         }
+        //
+        //         new Person(that._svg, that._configuration, person, d);
+        //     });
 
         this.bindClickEventListener();
         this.updateViewBox();
@@ -174,28 +166,27 @@ export default class Chart
      */
     bindClickEventListener()
     {
-        let persons = this._svg.get()
-            .select("g.personGroup")
+        let that = this;
+
+        this._svg.visual
             .selectAll("g.person")
             .filter((d) => d.data.xref !== "")
-            .classed("available", true);
-
-        // Trigger method on click
-        persons.on("click", this.personClick.bind(this));
+            .each(function (d) {
+                d3.select(this).on("click", function() { that.personClick(d.data); });
+            });
     }
 
     /**
      * Method triggers either the "update" or "individual" method on the click on an person.
      *
-     * @param {Event}  event The current event
-     * @param {Object} data  The D3 data object
+     * @param {Object} data The D3 data object
      *
      * @private
      */
-    personClick(event, data)
+    personClick(data)
     {
         // Trigger either "update" or "redirectToIndividual" method on click depending on person in chart
-        (data.depth === 0) ? this.redirectToIndividual(data.data.url) : this.update(data.data.updateUrl);
+        (data.generation === 1) ? this.redirectToIndividual(data.url) : this.update(data.updateUrl);
     }
 
     /**
@@ -217,8 +208,34 @@ export default class Chart
      */
     update(url)
     {
-        let update = new Update(this._svg, this._configuration, this._hierarchy);
-
-        update.update(url, () => this.bindClickEventListener());
+        window.location = url;
     }
+
+    // /**
+    //  * Changes root individual
+    //  *
+    //  * @param {String} url The update url
+    //  *
+    //  * @private
+    //  */
+    // update(url)
+    // {
+    //     var that = this;
+    //
+    //     $.getJSON(url, function(data) {
+    //         that.data = data;
+    //         that.draw();
+    //
+    //         var indSelector = $(document.getElementById('xref'));
+    //         $.ajax({
+    //             type: 'POST',
+    //             url: indSelector.attr("data-ajax--url"),
+    //             data: { q : data.xref }
+    //         }).then(function (data) {
+    //             // create the option and append to Select2
+    //             var option = new Option(data.results[0].text, data.results[0].id, true, true);
+    //             indSelector.append(option).trigger('change');
+    //         });
+    //     });
+    // }
 }
